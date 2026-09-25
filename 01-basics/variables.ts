66d191963c40b7cea7be7e165f5a497335274521
Rajeshwari-vegam <shivappa.rajeshwari@vegam.co>
2026-09-25 13:56:36 +0530

    Add TypeScript variables example

--- 01-basics/variables.ts
+++ 01-basics/variables.ts
@@ -1,7 +1,9 @@
 let userName: string = "RajeshwariS";
+let lastName: string = "shivappa";
 let age: number = 25;
 let isTester: boolean = true;
 
 console.log(userName);
+console.log(lastName);
 console.log(age);
 console.log(isTester);
